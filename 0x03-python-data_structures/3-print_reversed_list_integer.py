10716c3f6f32e7b2159e3effd74f19b18e7ad6c9
magululukas <morcusmagulu@gmail.com>
2021-11-24 11:15:43 +0000

    A list-reversing function.

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""areversive function"""
+def print_reversed_list_integer(my_list=[]):
+    my_list = [1, 2, 3, 4, 5]
+    r_list = my_list.reverse()
+    print("{:d}\n".format(r_list))
