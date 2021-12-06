34da9585c3b07e9791f933e3ff5149a2bdd49d32
magululukas <morcusmagulu@gmail.com>
2021-12-06 10:01:35 +0000

    First using of exceptions!

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""printing x elements of alist"""
+def safe_print_list(my_list=[], x=0):
+    my_list = [1, 2, 3, 4, 5, 6]
+    print('my_list')
+    for x in my_list:
+        print("My list contains: {:d} integers".format(x))
+try:
+    safe_print_list(my_list=[], x=0)
+except ValueError:
+    print("x is assigned wrongly")
+else:
+    print("known error not detected")
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,4 @@
+Hi There!
+You are most welcome to my folder.
+This folder is generally intended to contain programmes dealing with the use of "Exceptions" as they are known in python language.
+Cheers as you explore.!!!
