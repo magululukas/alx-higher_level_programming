3526b7593a90d4a92879ec7bc3ad76f21dd4c5ab
magululukas <morcusmagulu@gmail.com>
2021-12-06 10:12:06 +0000

    First using of exceptions! new line included

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     my_list = [1, 2, 3, 4, 5, 6]
     print('my_list')
     for x in my_list:
-        print("My list contains: {:d} integers".format(x))
+        print("My list contains: {:d} integers\n".format(x))
 try:
     safe_print_list(my_list=[], x=0)
 except ValueError:
