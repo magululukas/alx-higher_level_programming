60935a10248d4ff7307ba72878866dfa89424090
magululukas <morcusmagulu@gmail.com>
2021-12-17 12:40:36 +0000

    An element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,10 +2,10 @@
 def element_at(my_list, idx):
     """retrieve the element in a list"""
     my_list = [1, 2, 3, 4, 5, 6]
-    for idx in my_list:
-        idx = my_list[1]
+    for x in my_list:
+        idx = x[1]
     if idx < 0:
         return None
-    if idx > len(my_list):
+    if idx > len(x):
         return None
     print("the element at idx {:d} is {:d}\n".format(idx, element_at(my_list, idx)))
