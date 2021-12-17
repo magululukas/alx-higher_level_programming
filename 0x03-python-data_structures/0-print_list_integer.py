3d59524a7ff5e5f3749fb182af4a08cd4da7bc49
magululukas <morcusmagulu@gmail.com>
2021-12-17 09:12:02 +0000

    print all integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    integers = (range(5))
-    for my_list in integers: #variable x being assigned to alist of range
-        print("{:d}\n".format(my_list))
+    """print all integeers of a list."""
+    for x in my_list:
+        print("{:d}\n".format(x))
